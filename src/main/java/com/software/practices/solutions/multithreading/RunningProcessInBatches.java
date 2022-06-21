220b0d747201a60ae267cf69a352d84a8ccfa128
Sagar Kanojia <isagarkanojia@gmail.com>
2022-06-21 15:43:26 +0530

    Add: Running Process In Batches using MultiThreading Await

--- src/main/java/com/software/practices/solutions/multithreading/RunningProcessInBatches.java
+++ src/main/java/com/software/practices/solutions/multithreading/RunningProcessInBatches.java
@@ -2,11 +2,10 @@ package com.software.practices.solutions.multithreading;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author Sagar Kanojia
@@ -83,7 +82,7 @@ public class RunningProcessInBatches {
         System.out.println("Processed " + id);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         List<Integer> input = new ArrayList<>();
 
@@ -93,7 +92,59 @@ public class RunningProcessInBatches {
 
         RunningProcessInBatches process = new RunningProcessInBatches();
 
-        process.process(input);
+//        process.process(input);
+
+        process.process2(input);
     }
 
+    private void process2(List<Integer> ids) throws ExecutionException, InterruptedException {
+
+        List<Future> futures = new ArrayList<>();
+
+        int threadPoolSize = 10, bufferSize = 1;
+        int startIndex = 0;
+
+        int N = ids.size();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
+
+        try {
+            for (int thread = 0; thread < threadPoolSize; thread++) {
+                int endIndex = startIndex + bufferSize;
+                if (startIndex >= N) {
+                    break;
+                }
+                if (endIndex > N) {
+                    endIndex = N;
+                }
+
+                try {
+                    log.info("Process Batch Start {} End {}", startIndex, endIndex);
+
+                    List<Integer> batch = ids.subList(startIndex, endIndex);
+
+                    futures.add(executorService.submit(() -> processBatch(batch)));
+
+                    log.info("Success Batch Start {} End {}", startIndex, endIndex);
+
+                } catch (Exception e) {
+                    log.error("Error in Batch Start {} End {}", startIndex, endIndex);
+                }
+
+                startIndex = endIndex;
+
+            }
+        } finally {
+            System.out.println("CALLING SHUTDOWN");
+            executorService.shutdown();
+        }
+
+
+        for(Future f: futures) {
+            f.get();
+        }
+
+
+    }
+    
 }
