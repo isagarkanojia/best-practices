e4837b1c1bf735e01001f911c2c061e4b6d5287f
Sagar Kanojia <isagarkanojia@gmail.com>
2022-07-05 02:16:10 +0530

    Add: reflection solutions

--- src/main/java/com/software/practices/solutions/reflection/ReflectionV1.java
+++ src/main/java/com/software/practices/solutions/reflection/ReflectionV1.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Slf4j
 public class ReflectionV1 {
 
-    public static void main(String[] args) throws IntrospectionException, ReflectiveOperationException {
+    public static void main(String[] args)  {
 
         Data data = new Data().builder()
                 .name("Sagar")
